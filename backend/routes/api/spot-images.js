892e351aae883df3cf4ca65d37ddef775692ee4d
Angad Bhatia <128391290+Angad-Bhatia@users.noreply.github.com>
2023-06-19 07:21:34 -0400

    delete review image start

--- backend/routes/api/spot-images.js
+++ backend/routes/api/spot-images.js
@@ -10,9 +10,40 @@ const Op = Sequelize.Op;
 
 const router = express.Router();
 
-const { Spot, Review, SpotImage, ReviewImage, Booking } = require('../../db/models');
+const { Spot, SpotImage } = require('../../db/models');
 
+//DELETE (delete spot-images)
+router.delete('/:imageId', requireAuth, async (req, res, next) => {
+    const { user } = req;
+    const id = req.params.imageId;
+    const image = await SpotImage.findByPk(id, {
+        include: [
+            {
+                model: Spot,
+                attributes: ['ownerId']
+            }
+        ]
+    });
 
+    if (!image) {
+        const err = new Error("Spot Image couldn't be found");
+        err.title = 'Resource Not Found';
+        err.errors = { message: err.message };
+        err.status = 404;
+        return next(err);
+    }
+
+    if (user.id !== image.Spot.ownerId) {
+        const err = new Error('Spot does not belong to user');
+        err.title = 'Forbidden'
+        err.errors = { message: err.message }
+        err.status = 403;
+        return next(err);
+    }
+
+    await image.destroy();
+    res.json({ message: 'Successfully deleted' });
+});
 
 
 module.exports = router;
